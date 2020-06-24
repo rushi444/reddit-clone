fd914569ab32473acb9994e36cca0cfd989c1cc8
rushi444 <rushi44@gmail.com>
2020-06-24 11:54:46 -0700

    connected to db and server requests working

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+import * as bodyParser from 'body-parser'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post(`/user`, async (req, res) => {
+    const result = await prisma.user.create({
+        data: {
+            ...req.body,
+        },
+    })
+    res.json(result)
+})
+
+app.get(`/users`, async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.json(users)
+})
+
+
+app.listen(3000, () =>
+    console.log(
+        '🚀 Server ready at: http://localhost:3000\n⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api',
+    ),
+)
